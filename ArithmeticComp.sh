3af01bbb7c925d41bcac9d90299edec1dd758d15
paraglande03 <paraglande03@gmail.com>
2020-12-12 16:59:02 +0530

    Adding uc7

--- ArithmeticComp.sh
+++ ArithmeticComp.sh
@@ -7,3 +7,6 @@ Dict[res1]=$(($a+$b*$c))
 Dict[res2]=$(($a*$b+$c))
 Dict[res3]=$(($c+$a/$a))
 Dict[res4]=$(($a%$b+$c))
+
+arr=( ${Dict[res1]} ${Dict[res2]} ${Dict[res3]} ${Dict[res3]} ${Dict[res4]} )
+echo "${arr[@]}"
