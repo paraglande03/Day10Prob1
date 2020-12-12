1c1ffa7837d5e481a78aa8e2132fc324367ba174
paraglande03 <paraglande03@gmail.com>
2020-12-12 16:32:51 +0530

    Adding into dictionary

--- ArithmeticComp.sh
+++ ArithmeticComp.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
+
 read -p "Enter 3 number" a b c
-res1=$(($a+$b*$c))
-res2=$(($a*$b+$c))
-res3=$(($c+$a/$a))
-res4=$(($a%$b+$c))
+declare -A Dict
+
+Dict[res1]=$(($a+$b*$c))
+Dict[res2]=$(($a*$b+$c))
+Dict[res3]=$(($c+$a/$a))
+Dict[res4]=$(($a%$b+$c))
